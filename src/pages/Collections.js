c306729e18861922730bef0b930251500c9dfd31
Mandeep-Kaur27 <102357640+Mandeep-Kaur27@users.noreply.github.com>
2023-04-10 16:44:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/Collections.js
@@ -0,0 +1,508 @@
+import "./Collections.css";
+
+const Collections = () => {
+  return (
+    <div className="collections">
+      <footer className="footer-wrapper">
+        <div className="footer">
+          <div className="copyright-2023-wrapped">{`Copyright 2023 Wrapped Ordinals, Inc. `}</div>
+          <div className="social-media">
+            <div className="twitter">Twitter</div>
+            <div className="instagram">Instagram</div>
+            <div className="discord">Discord</div>
+            <div className="social-media1">Social Media</div>
+          </div>
+          <div className="comunity">
+            <div className="become-a-partner">Become a Partner</div>
+            <div className="faq-wrapper">
+              <div className="faq">FAQ</div>
+            </div>
+            <div className="collections1">Collections</div>
+            <div className="token">Token</div>
+            <div className="social-media1">Comunity</div>
+          </div>
+          <div className="about">
+            <div className="support">Support</div>
+            <div className="privacy-policy">Privacy Policy</div>
+            <div className="terms-of-service">Terms of Service</div>
+            <div className="about-us">About Us</div>
+            <div className="about1">About</div>
+          </div>
+          <div className="logo">
+            <div className="wrap-your-btc">
+              Wrap your BTC ordinals to enable trading on Opensea
+            </div>
+            <img className="logo-1-icon" alt="" src="/logo-1@2x.png" />
+          </div>
+        </div>
+      </footer>
+      <div className="cta-wrapper">
+        <div className="footer">
+          <div className="container" />
+          <div className="email">
+            <div className="button">
+              <b className="medium">Subscribe</b>
+            </div>
+            <div className="form">
+              <div className="text-field" />
+              <div className="your-email">Your Email</div>
+            </div>
+          </div>
+          <div className="lorem-ipsum-dolor">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor
+          </div>
+          <div className="stay-in-the">Stay in the loop</div>
+        </div>
+      </div>
+      <div className="exhibition-card">
+        <div className="pagination">
+          <div className="chevron-left-parent">
+            <img
+              className="chevron-left-icon"
+              alt=""
+              src="/16--chevronleft.svg"
+            />
+            <div className="group-child" />
+          </div>
+          <div className="rectangle-parent">
+            <div className="group-item" />
+            <div className="div">1</div>
+          </div>
+          <div className="rectangle-group">
+            <div className="group-inner" />
+            <div className="div1">2</div>
+          </div>
+          <div className="rectangle-group">
+            <div className="group-inner" />
+            <div className="div1">3</div>
+          </div>
+          <div className="rectangle-group">
+            <div className="group-inner" />
+            <div className="div1">4</div>
+          </div>
+          <div className="rectangle-group">
+            <div className="group-inner" />
+            <div className="div1">5</div>
+          </div>
+          <div className="chevron-left-parent">
+            <div className="group-child" />
+            <img
+              className="chevron-left-icon"
+              alt=""
+              src="/16--chevronright.svg"
+            />
+          </div>
+        </div>
+        <div className="exhibition-10">
+          <div className="featured-artist">
+            <div className="more-profile">
+              <b className="more">15+ More</b>
+              <img
+                className="image-placeholder-icon"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon1"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon2"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+            </div>
+            <div className="profile-5">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-4">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-3">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-2">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-1">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="featured-artist1">Featured Artist</div>
+          </div>
+          <div className="category">
+            <div className="blue-color">
+              <div className="container1" />
+              <b className="blue-color1">Blue Color</b>
+            </div>
+            <div className="cold">
+              <div className="container2" />
+              <b className="blue-color1">Crypto Art</b>
+            </div>
+            <div className="cold2">
+              <div className="container3" />
+              <b className="cold3">Blue Filter</b>
+            </div>
+            <div className="cold4">
+              <div className="container4" />
+              <b className="cold5">Cold</b>
+            </div>
+          </div>
+          <div className="a-fragmental-dispersion-container">
+            <p className="a-fragmental-dispersion">
+              A fragmental dispersion of the heart. I used many photoshop
+              geometrical brushes with different random size and opacity, in
+              order to emphatize the contrast between
+            </p>
+            <p className="a-fragmental-dispersion">
+              in order to emphatize the contrast between
+            </p>
+          </div>
+          <div className="nakomotos">Nakomotos</div>
+          <div className="june-162021">June 16,2021</div>
+          <div className="image-placeholder" />
+        </div>
+        <div className="exhibition-9">
+          <div className="featured-artist">
+            <div className="more-profile">
+              <b className="more">15+ More</b>
+              <img
+                className="image-placeholder-icon"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon1"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon2"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+            </div>
+            <div className="profile-5">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-4">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-3">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-2">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-1">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="featured-artist1">Featured Artist</div>
+          </div>
+          <div className="category">
+            <div className="blue-color">
+              <div className="container1" />
+              <b className="blue-color1">Blue Color</b>
+            </div>
+            <div className="cold">
+              <div className="container2" />
+              <b className="blue-color1">Crypto Art</b>
+            </div>
+            <div className="cold2">
+              <div className="container3" />
+              <b className="cold3">Blue Filter</b>
+            </div>
+            <div className="cold4">
+              <div className="container4" />
+              <b className="cold5">Cold</b>
+            </div>
+          </div>
+          <div className="a-fragmental-dispersion-container">
+            <p className="a-fragmental-dispersion">
+              A fragmental dispersion of the heart. I used many photoshop
+              geometrical brushes with different random size and opacity, in
+              order to emphatize the contrast between
+            </p>
+            <p className="a-fragmental-dispersion">
+              in order to emphatize the contrast between
+            </p>
+          </div>
+          <div className="nakomotos">Nakomotos</div>
+          <div className="june-162021">June 16,2021</div>
+          <div className="image-placeholder" />
+        </div>
+        <div className="exhibition-8">
+          <div className="featured-artist">
+            <div className="more-profile">
+              <b className="more">15+ More</b>
+              <img
+                className="image-placeholder-icon"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon1"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon2"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+            </div>
+            <div className="profile-5">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-4">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-3">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-2">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-1">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="featured-artist1">Featured Artist</div>
+          </div>
+          <div className="category">
+            <div className="blue-color">
+              <div className="container1" />
+              <b className="blue-color1">Blue Color</b>
+            </div>
+            <div className="cold">
+              <div className="container2" />
+              <b className="blue-color1">Crypto Art</b>
+            </div>
+            <div className="cold2">
+              <div className="container3" />
+              <b className="cold3">Blue Filter</b>
+            </div>
+            <div className="cold4">
+              <div className="container4" />
+              <b className="cold5">Cold</b>
+            </div>
+          </div>
+          <div className="a-fragmental-dispersion-container">
+            <p className="a-fragmental-dispersion">
+              A fragmental dispersion of the heart. I used many photoshop
+              geometrical brushes with different random size and opacity, in
+              order to emphatize the contrast between
+            </p>
+            <p className="a-fragmental-dispersion">
+              in order to emphatize the contrast between
+            </p>
+          </div>
+          <div className="nakomotos">Nakomotos</div>
+          <div className="june-162021">June 16,2021</div>
+          <div className="image-placeholder" />
+        </div>
+        <div className="exhibition-1">
+          <div className="featured-artist">
+            <div className="more-profile">
+              <b className="more">15+ More</b>
+              <img
+                className="image-placeholder-icon"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon1"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+              <img
+                className="image-placeholder-icon2"
+                alt=""
+                src="/image-placeholder.svg"
+              />
+            </div>
+            <div className="profile-5">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-4">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-3">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-2">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="profile-1">
+              <b className="theresa-webb">Theresa Webb</b>
+              <img
+                className="image-placeholder-icon3"
+                alt=""
+                src="/image-placeholder1.svg"
+              />
+            </div>
+            <div className="featured-artist1">Featured Artist</div>
+          </div>
+          <div className="category">
+            <div className="blue-color">
+              <div className="container1" />
+              <b className="blue-color1">Blue Color</b>
+            </div>
+            <div className="cold">
+              <div className="container2" />
+              <b className="blue-color1">Crypto Art</b>
+            </div>
+            <div className="cold2">
+              <div className="container3" />
+              <b className="cold3">Blue Filter</b>
+            </div>
+            <div className="cold4">
+              <div className="container4" />
+              <b className="cold5">Cold</b>
+            </div>
+          </div>
+          <div className="a-fragmental-dispersion-container">
+            <p className="a-fragmental-dispersion">
+              A fragmental dispersion of the heart. I used many photoshop
+              geometrical brushes with different random size and opacity, in
+              order to emphatize the contrast between
+            </p>
+            <p className="a-fragmental-dispersion">
+              in order to emphatize the contrast between
+            </p>
+          </div>
+          <div className="nakomotos">Nakomotos</div>
+          <div className="june-162021">June 16,2021</div>
+          <div className="image-placeholder" />
+        </div>
+      </div>
+      <div className="lorem-ipsum-is">
+        Lorem ipsum is a placeholder text commonly used to demonstrate the
+        visual form of a document or a typeface without relying on meaningful
+        content.
+      </div>
+      <div className="wror-collections">WROR Collections</div>
+      <div className="navbar">
+        <div className="container17" />
+        <div className="button1">
+          <b className="medium">Connect Wallet</b>
+        </div>
+        <div className="menu">
+          <div className="comunity2">
+            <div className="community">Community</div>
+            <img
+              className="arrow-chevron-down"
+              alt=""
+              src="/arrow-chevron-down.svg"
+            />
+          </div>
+          <div className="artist-collectors">{`Artist & Collectors`}</div>
+          <b className="exhibition">Exhibition</b>
+          <div className="explore">Explore</div>
+          <div className="market">Market</div>
+          <div className="home">Home</div>
+        </div>
+        <img className="logo-1-icon1" alt="" src="/logo-11@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Collections;
